801bbe3370794f8f526ac2801df9068f618d1b07
Jake Bromberg <jake.bromberg@gmail.com>
2015-10-31 14:10:04 -0700

    Initial commit

---
+++ Game of Life/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Jake on 10/31/15.
+//  Copyright © 2015 Flat Cap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
---
+++ Game of LifeTests/Game_of_LifeTests.swift
@@ -0,0 +1,14 @@
+//
+//  Game_of_LifeTests.swift
+//  Game of LifeTests
+//
+//  Created by Jake on 10/31/15.
+//  Copyright © 2015 Flat Cap. All rights reserved.
+//
+
+import XCTest
+@testable import Game_of_Life
+
+class Game_of_LifeTests: XCTestCase {
+        
+}
